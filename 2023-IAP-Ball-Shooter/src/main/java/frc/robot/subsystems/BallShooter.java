e6e30d9ee73e3abec9cae7fecb8c48d8e35a9f67
AnnaMariaIsaev <114833688+AnnaMariaIsaev@users.noreply.github.com>
2023-11-11 16:02:37 -0800

    jfdslaj

--- 2023-IAP-Ball-Shooter/src/main/java/frc/robot/subsystems/BallShooter.java
+++ 2023-IAP-Ball-Shooter/src/main/java/frc/robot/subsystems/BallShooter.java
@@ -2,14 +2,29 @@
 // Open Source Software; you can modify and/or share it under the terms of
 // the WPILib BSD license file in the root directory of this project.
 
-package frc.robot.commands;
+package frc.robot.subsystems;
 
+import edu.wpi.first.math.controller.PIDController;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 
-public class spinWheel extends CommandBase {
+import com.ctre.phoenix.motorcontrol.ControlMode;
+import com.ctre.phoenix.motorcontrol.FeedbackDevice;
+import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
+
+import edu.wpi.first.wpilibj.simulation.FlywheelSim;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+
+
+public class BallShooter extends CommandBase {
   /** Creates a new spinWheel. */
-  this.joy = joy;
-  public spinWheel() {
+  private static final double kP = 0;
+  private static final double kI = 0;
+  private static final double kD = 0;
+
+  private PIDController pid = new PIDController(0, 0, 0);
+
+  private WPI_TalonSRX flyWheel = new WPI_TalonSRX();
+  public BallShooter() {
     // Use addRequirements() here to declare subsystem dependencies.
   }
 
